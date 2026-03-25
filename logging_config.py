7cbde57b186f139f1a0b30b137205a603156ebb1
Bang Tran <btranmath@gmail.com>
2026-03-24 19:47:25 -0700

    Add logging configuration with CloudWatch support

---
+++ logging_config.py
@@ -0,0 +1,34 @@
+import logging
+import watchtower
+import boto3
+
+# Configure root logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Avoid duplicate handlers
+if not logger.handlers:
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+
+    cloudwatch_handler = watchtower.CloudWatchLogHandler(
+        boto3_session=boto3.Session(),
+        log_group="deepracer-logs",
+        stream_name="training-stream"
+    )
+    cloudwatch_handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+    '{"time": "%(asctime)s", "logger": "%(name)s", "level": "%(levelname)s", "message": "%(message)s"}'
+    )
+
+    console_handler.setFormatter(formatter)
+    cloudwatch_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+    logger.addHandler(cloudwatch_handler)
+
+
+def get_logger(name):
+    return logging.getLogger(name)
